055e4bf3673fe9f62b04ae792aa6ea752fbfcee0
DESKTOP-LER948U\Martin_Aldair <m.a.rmz.ramos@outlook.com>
2022-04-30 18:34:59 -0500

    Updates

---
+++ precache-manifest.a0c6e8b284fc21118390d1357ab98f5c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db66c32a2696a6610976eb1dd1457449",
+    "url": "/deploy-reactjs-echat-demo/index.html"
+  },
+  {
+    "revision": "2c81987ec29588d733e5",
+    "url": "/deploy-reactjs-echat-demo/static/css/3.eda5744e.chunk.css"
+  },
+  {
+    "revision": "ffb550f177000cc407f6",
+    "url": "/deploy-reactjs-echat-demo/static/css/main.35797f08.chunk.css"
+  },
+  {
+    "revision": "e5f0ea345826a3f87cde",
+    "url": "/deploy-reactjs-echat-demo/static/js/0.fbe10b7f.chunk.js"
+  },
+  {
+    "revision": "2c81987ec29588d733e5",
+    "url": "/deploy-reactjs-echat-demo/static/js/3.e86e7744.chunk.js"
+  },
+  {
+    "revision": "d6c4320175a8ef5df18a2514e420d120",
+    "url": "/deploy-reactjs-echat-demo/static/js/3.e86e7744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a329ee7cb5e6a2a0b86",
+    "url": "/deploy-reactjs-echat-demo/static/js/4.845ca4c8.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "/deploy-reactjs-echat-demo/static/js/4.845ca4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb550f177000cc407f6",
+    "url": "/deploy-reactjs-echat-demo/static/js/main.d0631832.chunk.js"
+  },
+  {
+    "revision": "3629eddb0e4a14016c00",
+    "url": "/deploy-reactjs-echat-demo/static/js/runtime-main.ae0fa16a.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "/deploy-reactjs-echat-demo/static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deploy-reactjs-echat-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deploy-reactjs-echat-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deploy-reactjs-echat-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deploy-reactjs-echat-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deploy-reactjs-echat-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/deploy-reactjs-echat-demo/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/deploy-reactjs-echat-demo/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/deploy-reactjs-echat-demo/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/deploy-reactjs-echat-demo/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/deploy-reactjs-echat-demo/static/media/primeicons.e01fd413.eot"
+  }
+]);
